8457f27f067a5f015668d9aee47252c2df91ed3c
Mongrell <justinufs@gmail.com>
2019-08-06 15:18:01 +0200

    Added first try

---
+++ First Try/versions.py
@@ -0,0 +1,20 @@
+#Check the versions of libraries
+
+#Python Version
+import sys
+print('Python: {}'.format(sys.version))
+#scipy
+import scipy
+print('SciPy: {}'.format(scipy.__version__))
+#numpy
+import numpy
+print('NumPy: {}'.format(numpy.__version__))
+#matplotlib
+import matplotlib
+print('MatPlotLib: {}'.format(matplotlib.__version__))
+#pandas
+import pandas
+print('Pandas: {}'.format(pandas.__version__))
+#sklearn
+import sklearn
+print('SkLearn: {}'.format(sklearn.__version__))
